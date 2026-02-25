f43c0c4b6de5b3792c4929ce20b150ca73060b12
Veronicah Kanja <kanjaveronicah@gmail.com>
2026-02-25 18:54:45 +0300

    commplete lab

--- server/app.py
+++ server/app.py
@@ -16,11 +16,51 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([m.to_dict() for m in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+
+    db.session.add(new_message)
+    db.session.commit()
+
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    data = request.get_json()
+
+    message.body = data.get('body', message.body)
+
+    db.session.commit()
+
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+
+    message = Message.query.get(id)
+
+    if not message:
+        return jsonify({"error": "Message not found"}), 404
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return jsonify({"message": "Deleted successfully"}), 200
 
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from datetime import datetime
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
+
 
 class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
+    serialize_rules = ()
+
     id = db.Column(db.Integer, primary_key=True)
+
+    body = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
